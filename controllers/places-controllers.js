f6b3aef5e2a18735594b9db923d5293868ba22ca
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-03 14:23:49 +0530

    Created Place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
-const Place = require('../models/place');
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -65,16 +65,25 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image: "https://picsum.photos/id/1/200/300",
     creator,
-  };
-  DUMMY_PLACES.push(createdPlace);
+  });
+
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
   res.status(201).json({ place: createdPlace });
 };
 
