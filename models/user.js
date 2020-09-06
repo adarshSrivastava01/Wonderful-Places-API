d7ecd0d483d703fd7b1556f69a202e56f6005ef1
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-06 17:06:49 +0530

    Started Connecting

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  password: { type: String, required: true, minLength: 6 },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
