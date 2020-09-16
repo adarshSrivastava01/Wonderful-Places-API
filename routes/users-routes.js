cb0a3990a2f4a0964665773c34271d7604a39625
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-16 17:25:49 +0530

    Multer added

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const HttpError = require("../models/http-error");
 
@@ -11,6 +12,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
