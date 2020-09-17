c97079090aa939af24ceb892076da483f9387d6f
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-17 08:26:37 +0530

    Authentication Done

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -3,6 +3,7 @@ const { check } = require("express-validator");
 
 const placesControllers = require("../controllers/places-controllers");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const HttpError = require("../models/http-error");
 
@@ -11,6 +12,8 @@ const router = express.Router();
 router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post(
   "/",
